4a932e581bef8bc3708f4e74a6ccc64666c921e2
Jayant Kumar <jk0256707@gmail.com>
2025-08-12 00:10:51 +0530

    added form valdation and monitor_and_scale logic

--- scheduler_core/forms.py
+++ scheduler_core/forms.py
@@ -2,6 +2,14 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
+
+    def clean_scheduled_time(self):
+        from django.utils import timezone
+        scheduled_time = self.cleaned_data['scheduled_time']
+        if scheduled_time <= timezone.now():
+            raise forms.ValidationError("Scheduled time must be in the future.")
+        return scheduled_time
+    
     scheduled_time = forms.DateTimeField(
         widget=forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
         input_formats=['%Y-%m-%dT%H:%M'],
